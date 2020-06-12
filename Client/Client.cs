848c0c703f51e93fdcf03189e7afe7c20109c7c4
David Ramos <dramos97.dr24@gmail.com>
2020-06-12 17:21:02 +0200

    #1 Connect both, Server and Client through a xml, firt step

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,11 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace adm
 {
     class Client
     {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Client Application");
+            while (true)
+            {
+                try
+                {
+
+                    TcpClient tcpclnt = new TcpClient();
+                    Console.WriteLine("Conectando.....");
+                    // utilizar para este caso IP local ya que 
+                    // cliente y servidor corren en la misma PC 
+                    tcpclnt.Connect("127.0.0.1", 8001);
+                    Console.WriteLine("*** Conectado con el servidor ***");
+                    Console.Write("Introduzca frase a transmitir: ");
+                    String str = Console.ReadLine();
+                    Stream stm = tcpclnt.GetStream();
+                    
+                    // convertir cadena a ascii para transmitirla 
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] ba = asen.GetBytes(str);
+                    Console.WriteLine("Transmitiendo cadena...");
+                    stm.Write(ba, 0, ba.Length);
+                    // recibir acuse, se debe converir a string 
+                    byte[] bb = new byte[100];
+                    int k = stm.Read(bb, 0, 100);
+                    string acuse = "";
+                    for (int i = 0; i < k; i++)
+                        acuse = acuse + Convert.ToChar(bb[i]);
+                    Console.WriteLine(acuse);
+                    tcpclnt.Close();
+                }
+                
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error... " + e.StackTrace);
+                }
+            }
+        }
+
+        /*
         public const string Error = "ERROR: ";
         public const string SecurityIncorrectLogin = Error + "Incorrect login";
         public const string SecurityNotSufficientPrivileges = Error + "Not sufficient privileges";
@@ -14,6 +62,7 @@ namespace adm
         public const string SecurityProfileDoesNotExist = Error + "Security profile does not exist";
         public const string SecurityUserDoesNotExist = Error + "Security user does not exist";
         public static string userDB="";
+       
         static string GetPassword(string passMsg)
         {
             StringBuilder password = new StringBuilder("");
@@ -347,7 +396,7 @@ namespace adm
             }
 
         
-
+            */
 
 
     }
